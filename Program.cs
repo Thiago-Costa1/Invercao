8925021eebf94ca5e1ebee1d3c0358ba470d9c72
Thiago <thiagocostas2017@gmail.com>
2020-11-25 11:53:49 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exc7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {double[] numeros_digitados = new double[15];
+            for (var i = 0; i <= 14; i++)      
+                {Console.WriteLine();
+                Console.WriteLine($"Escreva o {i+1}° número:\n ");
+                numeros_digitados[i] = double.Parse(Console.ReadLine());}
+                Console.WriteLine("Os números foram invertidos e ficaram assim:\n ");
+            for (var i = 14; i >= 0; i--)
+            {Console.WriteLine();
+             Console.WriteLine(numeros_digitados[i]);}     
+            }
+        }
+    }
+}
